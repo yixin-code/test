5f34a5a8d583beb34df97a73eda944d37071d4e6
yixin <yixincode@163.com>
2024-02-25 13:35:10 +0800

    13 35

--- 4.cpp
+++ 4.cpp
@@ -2,12 +2,21 @@
 
 class A {
 public:
+    A() = default;
+
     void display() {
-        std::cout << "m_c_num: " << m_c_num << "\n";
+        std::cout << "m_c_num: " << m_c_num
+        << ", num = " << m_num
+        << ", num2 = " << m_num2
+        << ", num3 = " << m_num3
+        << "\n";
     }
 
 private:
     const int m_c_num = 11;
+    int m_num;
+    int m_num2;
+    int m_num3;
 };
 
 int main() {
