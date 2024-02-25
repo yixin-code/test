82876509260f07e28f9cc6c550299fbdabdda675
yixin <yixincode@163.com>
2024-02-26 00:03:49 +0800

    0003

---
+++ 6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    float f = 1.1;
+
+    std::cout << sizeof(f + f) << "\n";
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 TARGET=a.out
 
-SRC=./5.c
+SRC=./6.cpp
 
 # 将.c替换成.o
 # SRC=$(patsubst %.cpp, %.o, $(TEMP))
