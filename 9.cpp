46aa8e87de23092c34069dc41b954fc839c4ca55
yixin <yixincode@163.com>
2024-03-02 10:36:30 +0800

    9

---
+++ 9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    goto fly;
+
+    // int num = 11;
+    // std::cout << "num: " << num << std::endl;
+    std::cout << "hello yixin" << std::endl;
+
+fly:
+    std::cout << "hello world" << std::endl;
+
+    return 0;
+}
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 TARGET=a.out
 
-SRC=./8.cpp
+SRC=./9.cpp
 
 # 将.c替换成.o
 # SRC=$(patsubst %.cpp, %.o, $(TEMP))
