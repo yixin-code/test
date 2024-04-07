7bd197ec177f4011e03da9413dd8ee43543e37ac
yixin <yixincode@163.com>
2024-04-08 00:05:03 +0800

    56

--- 56.cpp
+++ 56.cpp
@@ -4,7 +4,7 @@ int main() {
     int arr[] = {1, 2};
     int *p = arr;
     std::cout << *p << '\n';
-    std::cout << *(p + 1) << '\n';
+    std::cout << *(p + 1) < '\n';
     // std::cout << *(p + 2) << '\n';
 
     return 0;
