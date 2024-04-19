b3cfe3d9b7e5999620a281aa1b9c1f66ee9a4c56
yixin <yixincode@163.com>
2024-04-19 19:11:14 +0800

    61

--- 61.cpp
+++ 61.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <stdarg.h>
 
-void func(int num, ...) {
-    va_list ap;
-    va_start(ap, num);
+// void func(int num, ...) {
+//     va_list ap;
+//     va_start(ap, num);
 
-    while (num-- != 0) {
-        std::cout << va_arg(ap, int64_t) << '\n';
-    }
+//     while (num-- != 0) {
+//         std::cout << va_arg(ap, int64_t) << '\n';
+//     }
 
-    va_end(ap);
-}
+//     va_end(ap);
+// }
 
 int main() {
-    func(3, 11, 22, 33);
+    double num = 1;
+    printf("%.2lf\n", num);
+    // func(3, 11, 22, 33);
     // int num = 11;
     // printf("%%%d\n", num);
     // while (true) {
