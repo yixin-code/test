54aed22a73be907a7b9193730a009dc15fbc0cfe
yixin <yixincode@163.com>
2024-05-17 15:40:46 +0800

    65

--- 65.cpp
+++ 65.cpp
@@ -5,14 +5,21 @@
 int main(int argc, char *argv[]) {
     // std::fstream fio("65.txt", std::ios::in | std::ios::out);
     // std::fstream fio("65.txt", std::ios::out | std::ios::in);
-    std::ofstream fio("65.txt", std::ios::out | std::ios::in | std::ios::trunc);
+    std::ifstream fin("65.txt");
     // std::ofstream fout("65.txt", std::ios::w)
-    if (!fio.is_open()) {
-        perror("fio");
+    if (!fin.is_open()) {
+        perror("fin");
         exit(1);
     }
-    fio << "abc";
-    fio.close();
+    char buf[1024] = {0};
+    std::streamsize size = fin.read(buf, sizeof(buf)).gcount();
+    // fin.getline(buf, sizeof(buf)).gcount();
+    std::cout << size << std::endl;
+    fin.close();
+    std::ofstream fout("65.txt");
+    fout << "123";
+    fout.write("123", 3);
+
     // char buf[10] = {0};
     // int count = read(STDIN_FILENO, buf, sizeof(buf));
     // write(STDOUT_FILENO, buf, count);
