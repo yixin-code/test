fb8908d9cb25f3ffcf8943de1fcf48c32c24b008
yixin <yixincode@163.com>
2024-05-21 15:50:14 +0800

    66

--- 65.cpp
+++ 65.cpp
@@ -1,24 +1,26 @@
 #include <unistd.h>
 #include <iostream>
 #include <fstream>
+// #include "66.cpp"
 
 int main(int argc, char *argv[]) {
+    func();
     // std::fstream fio("65.txt", std::ios::in | std::ios::out);
     // std::fstream fio("65.txt", std::ios::out | std::ios::in);
-    std::ifstream fin("65.txt");
-    // std::ofstream fout("65.txt", std::ios::w)
-    if (!fin.is_open()) {
-        perror("fin");
-        exit(1);
-    }
-    char buf[1024] = {0};
-    std::streamsize size = fin.read(buf, sizeof(buf)).gcount();
-    // fin.getline(buf, sizeof(buf)).gcount();
-    std::cout << size << std::endl;
-    fin.close();
-    std::ofstream fout("65.txt");
-    fout << "123";
-    fout.write("123", 3);
+    // std::ifstream fin("65.txt");
+    // // std::ofstream fout("65.txt", std::ios::w)
+    // if (!fin.is_open()) {
+    //     perror("fin");
+    //     exit(1);
+    // }
+    // char buf[1024] = {0};
+    // std::streamsize size = fin.read(buf, sizeof(buf)).gcount();
+    // // fin.getline(buf, sizeof(buf)).gcount();
+    // std::cout << size << std::endl;
+    // fin.close();
+    // std::ofstream fout("65.txt");
+    // fout << "123";
+    // fout.write("123", 3);
 
     // char buf[10] = {0};
     // int count = read(STDIN_FILENO, buf, sizeof(buf));
--- 66.cpp
+++ 66.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    char buf[] = "abc";
-    char *p = buf;
-    char *p2 = buf + 1;
-    std::cout << p << std::endl;
-    std::cout << p2 << std::endl;
-    std::cout << p2 - p << std::endl;
-
-    return 0;
+static void func() {
+    std::cout << "static void func()\n";
 }
+
+// int main(int argc, char *argv[]) {
+//     func();
+//     // std::cout << strerror(2) << std::endl;
+//     // char buf[] = "abc";
+//     // char *p = buf;
+//     // char *p2 = buf + 1;
+//     // std::cout << p << std::endl;
+//     // std::cout << p2 << std::endl;
+//     // std::cout << p2 - p << std::endl;
+//     std::cout << "main end\n";
+
+//     return 0;
+// }
