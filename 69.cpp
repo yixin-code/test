d965548ad85c5b7ca48d724b2528272443918c75
yixin <yixincode@163.com>
2024-06-09 04:19:50 +0800

    69

--- 69.cpp
+++ 69.cpp
@@ -1,12 +1,16 @@
 #include <cstdlib>
 #include <iostream>
 #include <pthread.h>
+#include <semaphore.h>
 
 int main() {
     // system("warpd");
     // const pthread_mutex_t mutex;
+    sem_t   sem;
+    sem_init(&sem, 0, 1);
+    sem_wait(&sem);
 
-
+    std::cout << "main\n";
 
     return 0;
 }
