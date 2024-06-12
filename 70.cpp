5fbba7cfa1a4d8c96ff11f0d40135c3b9992d472
yixin <yixincode@163.com>
2024-06-12 18:52:18 +0800

    70

---
+++ 70.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int num     = 0;
+    int num2    = 0;
+
+    std::cin >> num;
+    std::cin >> num2;
+
+    std::cout << num + num2 << std::endl;
+
+    return 0;
+}
