2f235196dda541976353288ff05da82537467329
yixin <yixincode@163.com>
2024-06-15 05:06:48 +0800

    71

--- 71.cpp
+++ 71.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    int 
+    int arr[] {1, 2, 3, 4, 5, 5, 4, 3, 2, 1};
 
+    for (const auto val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
 
     return 0;
 }
