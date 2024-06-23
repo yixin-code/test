d065b8453c8a16afe2d48db316a0436a85ab8cd6
yixin <yixincode@163.com>
2024-06-24 04:52:54 +0800

    74

--- 74.cpp
+++ 74.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <queue>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    std::queue<int> q;
-    if (q.empty()) {
-        std::cout << "q is empty\n";
-    }
-    
+    // std::queue<int> q;
+    // if (q.empty()) {
+    //     std::cout << "q is empty\n";
+    // }
+    char buf[1024] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    read(STDIN_FILENO, buf, sizeof(buf));
+    std::cout << buf << "\n";
+
     return 0;
 }
