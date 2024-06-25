50a28a620e47bafdd89830d3d75b0da3727e7eb8
yixin <yixincode@163.com>
2024-06-25 08:42:42 +0800

    76

--- 76.cpp
+++ 76.cpp
@@ -2,11 +2,20 @@
 #include <string.h>
 
 int main() {
-    char buf[1024] = {0};
-    char buf2[] = "asdf";
-    strcat(buf, buf2);
+    FILE *fp = fopen("./76", "w+");
+    fputs("     asd    \n", fp);
+    char buf[1024] = "bbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+    fseek(fp, 0, SEEK_SET);
+    fgets(buf, 1024, fp);
+    std::cout << "[" << buf << "]";
+    if (isspace(*buf)) {
+        std::cout << "*buf is space" << std::endl;
+    }
+    // char buf[1024] = {0};
+    // char buf2[] = "asdf";
+    // strcat(buf, buf2);
 
-    std::cout << buf << std::endl;
+    // std::cout << buf << std::endl;
 
     return 0;
 }
