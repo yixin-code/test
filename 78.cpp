ea46858f748a3811f8fc096ca1e612681adb0154
yixin <yixincode@163.com>
2024-07-06 16:25:19 +0800

    78

--- 78.cpp
+++ 78.cpp
@@ -2,12 +2,26 @@
 #include <dirent.h>
 
 int main(int argc, char *argv[]) {
-    DIR     *p  = opendir("/home/yixin/Code/cpp");
-    dirent  *p2 = readdir(p);
-    std::cout << p2->d_name << "\n";
-    std::cout << p2->d_name << "\n";
-    std::cout << p2->d_name << "\n";
-    closedir(p);
+    if (true) {
+        goto fly;
+    }
+
+    if (true) {
+        int num = 11;
+        std::cout << "num: " << num << "\n";
+    }
+
+    for (u_int32_t i = 0; i < 11; i++) {
+    }
+
+fly:
+    std::cout << "fly\n";
+    // DIR     *p  = opendir("/home/yixin/Code/cpp");
+    // dirent  *p2 = readdir(p);
+    // std::cout << p2->d_name << "\n";
+    // std::cout << p2->d_name << "\n";
+    // std::cout << p2->d_name << "\n";
+    // closedir(p);
 
     return 0;
 }
