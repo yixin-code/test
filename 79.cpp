66b02b2738665b12933feb1813c171c75caa06e8
yixin <yixincode@163.com>
2024-08-14 17:16:25 +0800

    pthread_cleanup_push

--- 79.cpp
+++ 79.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
+
+void clean_func(void*) {
+    std::cout << "clean_func\n";
+    return;
+}
+
+void *thread_func(void*) {
+    pthread_exit(nullptr);
+    pthread_cleanup_push(clean_func, nullptr);
+
+    while (true) {
+        pthread_testcancel();
+        std::cout << "thread_func\n";
+        sleep(1);
+    }
+
+    pthread_cleanup_pop(0);
+
+    pthread_exit(nullptr);
+}
 
 int main(int argc, char *argv[]) {
+    pthread_t tid = 0;
+    pthread_create(&tid, nullptr, thread_func, nullptr);
+
+    sleep(3);
+
+    if (pthread_cancel(tid) != 0) {
+        std::cout << "pthread_cancel\n";
+    }
+
+    pthread_join(tid, nullptr);
+
+    std::cout << "main end\n";
+
     
     // execlp("./78", "./78", nullptr);
 
